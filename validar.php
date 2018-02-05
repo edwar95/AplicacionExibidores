022974f6d644fb8a16a5324b923d78f9ceeae26a
Unknown <davidmoralesp1995@gmail.com>
2018-02-05 10:39:13 -0500

    Login y principal OK

--- validar.php
+++ validar.php
@@ -1,18 +1,18 @@
 <?php
 $usuario=$_POST['usuario'];
-$contraseña=$_POST['contrasenia'];
+$contrasenia=$_POST['contrasenia'];
 $tipo=$_POST['tipo'];
 
 $conexion=new mysqli ("localhost","davidMorales","Edward10*","exibidores_db");
-$consulta=$conexion->query("SELECT * FROM usuarios WHERE nombreUser='$usuario' and password='$contraseña'and tipoUsuario='$tipo'") or die ("no sirve");
+$consulta=$conexion->query("SELECT * FROM usuarios WHERE nombreUser='$usuario' and password='$contrasenia'and tipoUsuario='$tipo'") or die ("no sirve");
 
 
 $filas=mysqli_num_rows($consulta);
 if($filas>0 && $tipo=="Administrador"){
-  header("location:principalAdmin.html");
+  header("location:principal.html");
 }
 else if ($filas>0 && $tipo=="Vendedor"){
-  header("location:principalVendedor.html");
+  header("location:principal.html");
 }
 mysqli_free_result($consulta);
 mysqli_close($conexion);
